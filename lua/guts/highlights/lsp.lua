a834d0f06a7439594f8ab76635dcaa9440b68aff
WoutVossen <vossen.w@hotmail.com>
2026-08-04 20:39:45 +0200

    Treat rust structs, enums as entities

--- lua/guts/highlights/lsp.lua
+++ lua/guts/highlights/lsp.lua
@@ -2,8 +2,10 @@ local M = {}
 
 function M.load(theme)
 	return {
-		["@lsp.type.class"]     = { fg = theme.entity },
-		["@lsp.type.parameter"] = { fg = theme.parameter },
+		["@lsp.type.class"]       = { fg = theme.entity },
+		["@lsp.type.parameter"]   = { fg = theme.parameter },
+		["@lsp.type.struct.rust"] = { fg = theme.entity },
+		["@lsp.type.enum.rust"]   = { fg = theme.entity },
 	}
 end
 
